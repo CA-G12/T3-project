12ac9a5a624626c7f63a0fd6c599fbcd54389a9f
Mohammed alagha <modyagha7@gmail.com>
2022-09-21 17:06:53 +0300

    just create the file setup :) close #1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Container from "./Components/Container";
+
+function App() {
+  return (
+    <div className="App">
+      <Container url="https://free-football-soccer-videos.p.rapidapi.com/" />
+    </div>
+  );
+}
+
+export default App;
